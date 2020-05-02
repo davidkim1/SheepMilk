b4a94b44d0ee6d4882359b056323848cf50d80be
David Kim Brady <davidekimb15@gmail.com>
2020-05-02 17:55:08 -0400

    First Commit

---
+++ android/src/com/sheepmilk/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.sheepmilk.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.sheepmilk.game.SheepMilk;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SheepMilk(), config);
+	}
+}
