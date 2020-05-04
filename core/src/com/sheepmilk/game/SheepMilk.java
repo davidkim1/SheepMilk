080c9944e1cc208f4008eb91e9bbef2d8d7bd048
David Kim Brady <davidekimb15@gmail.com>
2020-05-03 23:45:47 -0400

    Working

--- core/src/com/sheepmilk/game/SheepMilk.java
+++ core/src/com/sheepmilk/game/SheepMilk.java
@@ -2,32 +2,46 @@ package com.sheepmilk.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.sheepmilk.game.states.GameStateManager;
+import com.sheepmilk.game.states.MenuState;
 
 public class SheepMilk extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+	public static final int WIDTH = 480;
+	public static final int HEIGHT = 800;
+
+	public static final String TITLE = "Sheep Milk";
+	private GameStateManager gsm;
+	private SpriteBatch batch;
+
+	private Music music;
+
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		gsm = new GameStateManager();
+		music = Gdx.audio.newMusic(Gdx.files.internal("music.mp3"));
+		music.setLooping(true);
+		music.setVolume(0.1f);
+		music.play();
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		gsm.push(new MenuState(gsm));
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		gsm.update(Gdx.graphics.getDeltaTime());
+		gsm.render(batch);
 	}
-	
+
 	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+	public void dispose() {
+		super.dispose();
+		music.dispose();
 	}
+
 }
---
+++ core/src/com/sheepmilk/game/sprites/Bird.java
@@ -0,0 +1,68 @@
+package com.sheepmilk.game.sprites;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector3;
+
+import org.w3c.dom.Text;
+import org.w3c.dom.css.Rect;
+
+public class Bird {
+    private static final int GRAVITY = -15;
+    private static final int MOVEMENT = 100;
+    private Vector3 position;
+    private Vector3 velocity;
+    private Rectangle bounds;
+    //private Animation birdAnimation;
+    private Texture bird;
+    private Sound flap;
+
+    public Bird(int x, int y){
+        position = new Vector3(x, y, 0);
+        velocity = new Vector3(0, 0, 0);
+        bird = new Texture("bird.png");
+        //texture = new Texture("birdanimation.png");
+        //birdAnimation = new Animation(new TextureRegion(texture), 3, 0.5f);
+        bounds = new Rectangle(x, y, bird.getWidth(), bird.getHeight());
+        flap = Gdx.audio.newSound(Gdx.files.internal("sfx_wing.ogg"));
+    }
+
+    public void update(float dt){
+        // birdAnimation.update(dt);
+        if(position.y > 0)
+            velocity.add(0, GRAVITY, 0);
+        velocity.scl(dt);
+        position.add(MOVEMENT * dt, velocity.y, 0);
+        if(position.y < 0)
+            position.y = 0;
+
+        velocity.scl(1/dt);
+        bounds.setPosition(position.x, position.y);
+    }
+
+    public Vector3 getPosition() {
+        return position;
+    }
+
+    public Texture getTexture() {
+        return bird;
+    }
+
+    public void jump(){
+        velocity.y = 250;
+        flap.play(0.5f);
+    }
+
+    public Rectangle getBounds(){
+        return bounds;
+    }
+
+    public void dispose(){
+        bird.dispose();
+        flap.dispose();
+    }
+
+}
